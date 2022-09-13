2d1f561ba1e092564b1c5361ea0f580420499a59
DennisLCastillo <55577545+DennisLCastillo@users.noreply.github.com>
2022-09-13 13:10:46 +0200

    Add menu i Main Class

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,7 +1,8 @@
 public class Database {
     private SuperHero[] heroDatabase = new SuperHero[5];
 
-    public void addASuperhero(String name, String superheroName, String power, int year, double strength) {
-    SuperHero hero = new SuperHero(name, superheroName, power, year, strength);
+    public void addSuperhero(String name, String superheroName, String power, int year, double strength) {
+        SuperHero hero = new SuperHero(name, superheroName, power, year, strength);
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner valgScanner = new Scanner(System.in);
         Database heroDatabase = new Database();
 
-        System.out.println("Velkommen til Superhelte-Programmet");
+        int menu = 0;
+        do {
+            System.out.println("Velkommen til Superhelte-Programmet");
+            System.out.println("Her skal du tilføje din favorit superhelte");
+            System.out.println("""
+                    ---------------------------------------------
+                    1. Opret superhelt
+                    9. Afslut program""");
+            menu = valgScanner.nextInt();
+            valgScanner.nextLine();
+            if (menu == 1) {
+                System.out.println("Indtast superheltens originale navn: ");
+                Scanner scanner = new Scanner(System.in);
+                String name = scanner.nextLine();
+
+                System.out.println("Indtast superheltens navn: ");
+                String superheroName = scanner.nextLine();
+
+                System.out.println("Indtast superheltens superkræfter: ");
+                String power = scanner.nextLine();
+
+                System.out.println("Indtast superheltens udgivelsesår: ");
+                int year = scanner.nextInt();
+
+                System.out.println("Indtast superheltens styrke: ");
+                double strength = scanner.nextDouble();
+
+                heroDatabase.addSuperhero(name,superheroName,power,year,strength);
+            } else {
+                System.exit(0);
+            }
+        } while (menu != 9);
+
+
+
+
+
+       // heroDatabase.addSuperhero(name,superheroName,power,year,strength);
+    }
+}
+
+
+/* System.out.println("Velkommen til Superhelte-Programmet");
         System.out.println("Her skal du tilføje din favorit superhelte");
         System.out.println("---------------------------------------------");
 
@@ -23,8 +66,4 @@ public class Main {
         int year = scanner.nextInt();
 
         System.out.println("Indtast superheltens styrke: ");
-        double strength = scanner.nextDouble();
-
-        heroDatabase.addASuperhero(name,superheroName,power,year,strength);
-    }
-}
+        double strength = scanner.nextDouble();*/
